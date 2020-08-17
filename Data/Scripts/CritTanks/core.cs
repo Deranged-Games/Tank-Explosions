20539906b0fdfcc5c2fa75ccfaa94dd1894e467a
Dondelium <Dondelium@gmail.com>
2020-08-16 23:40:39 -0400

    initial commit

---
+++ Data/Scripts/CritTanks/core.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using System;
+using VRageMath;
+
+using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Common.ObjectBuilders.Definitions;
+using Sandbox.Definitions;
+using Sandbox.Game.ParticleEffects;
+using Sandbox.Game.Entities;
+using Sandbox.Game.GameSystems;
+using Sandbox.Game;
+using Sandbox.Definitions;
+
+using VRage;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Game.ModAPI.Interfaces;
+using VRage.Library.Utils;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+
+namespace Dondelium.crittanks{
+	[MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
+	public class CriticalExplode : MySessionComponentBase{
+    bool hasinit = false;
+    bool isServer = false;
+    MyRandom rand = new MyRandom();
+    
+		public override void UpdateBeforeSimulation(){
+			if (!hasinit){
+				if (MyAPIGateway.Session == null) return;
+				Init();
+			}
+		}
+    
+		private void Init(){
+			hasinit = true;
+			isServer = MyAPIGateway.Session.OnlineMode == MyOnlineModeEnum.OFFLINE || MyAPIGateway.Multiplayer.IsServer;
+			if(isServer) MyAPIGateway.Session.DamageSystem.RegisterBeforeDamageHandler(0, damageHook);
+    }
+
+    private void damageHook(object target, ref MyDamageInformation info){
+      if (info.Type==MyDamageType.Grind) return;
+
+			if (target is IMySlimBlock){
+				var entity = target as IMySlimBlock;{
+					if (entity.FatBlock is IMyGasTank){
+            var tBlock = entity.FatBlock as IMyTerminalBlock;
+            var def = entity.BlockDefinition as MyCubeBlockDefinition;
+            var grid = tBlock.CubeGrid as IMyCubeGrid;
+            var gasBlock = entity.FatBlock as IMyGasTank;
+            Vector3D myPos = gasBlock.GetPosition();
+
+            float critdmg = (1f - def.CriticalIntegrityRatio) * entity.MaxIntegrity;
+            float curDmg = info.Amount + entity.CurrentDamage;
+
+            if(tBlock.IsWorking && curDmg > critdmg){
+              float dmg = (gasBlock.Capacity * (float)gasBlock.FilledRatio * 0.006f / grid.GridSize);
+
+              //Check atmospheres. If we find oxygen, bigger boom, else, much smaller boom.
+              bool airtight = false;
+              try{
+                airtight = grid.IsRoomAtPositionAirtight(gasBlock.Position);
+              } catch(Exception e){}
+              if(airtight){
+                dmg *= 5f;
+              } else {
+                foreach(var kv in PlanetStorage.planets){
+                  var planet = kv.Value;
+                  if(planet.Closed || planet.MarkedForClose || !planet.HasAtmosphere){
+                    continue;
+                  }
+                  if(Vector3D.DistanceSquared(myPos, planet.WorldMatrix.Translation) < (planet.AtmosphereRadius * planet.AtmosphereRadius)){
+                    dmg *= 5f * planet.GetAirDensity(myPos);
+                    break;
+                  }
+                }
+              }
+
+              //Shrapnel from remaining tank integrity
+              float shrapnel = 0f;
+              if(entity.MaxIntegrity > curDmg && (float)gasBlock.FilledRatio >= 0.5f)
+                shrapnel = (entity.MaxIntegrity - curDmg) / 4;
+              dmg += shrapnel;
+
+              //Potential explosion damage less than remaining tank health? Cancel explosion, and vent!
+              if((dmg * 2.5f) < (entity.MaxIntegrity - curDmg)){
+                if(grid.Physics == null || grid.Physics.IsStatic)
+                  return;
+                float x = rand.NextFloat() * 2 - 1;
+                float y = rand.NextFloat() * 2 - 1;
+                float z = rand.NextFloat() * 2 - 1;
+                Vector3 thrustVec = new Vector3(x, y, z);
+                grid.Physics.AddForce(MyPhysicsForceType.APPLY_WORLD_FORCE, Vector3.Normalize(thrustVec) * (gasBlock.Capacity * (float)gasBlock.FilledRatio * 20), myPos, null);
+                return;
+              }
+
+              //Finalize damage and radius
+              float radius = dmg / 2000f;
+              if(dmg > 500000f) dmg = 500000f;
+              if(radius > 100f) radius = 100f;
+
+              //Explosion Damage!
+              BoundingSphereD sphere = new BoundingSphereD(myPos, radius);
+              MyExplosionInfo bomb = new MyExplosionInfo(dmg, dmg, sphere, MyExplosionTypeEnum.BOMB_EXPLOSION, true, true);
+              bomb.CreateParticleEffect = true;
+              bomb.LifespanMiliseconds = 150 + (int)radius * 45;
+              MyExplosions.AddExplosion(ref bomb, true);
+
+              //MyAPIGateway.Utilities.ShowNotification(dmg.ToString()+" "+radius.ToString(), 15000, MyFontEnum.Red);
+            }
+					}
+				}
+			}
+    }
+
+		protected override void UnloadData(){}
+  }
+}
---
+++ Data/Scripts/CritTanks/planets.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Sandbox.Common;
+using Sandbox.Common.Components;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Definitions;
+using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Gui;
+using Sandbox.Game;
+using VRage.Common.Utils;
+using VRageMath;
+using VRage;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.ObjectBuilders;
+using VRage.ModAPI;
+using VRage.Utils;
+using VRage.Library.Utils;
+
+using System.Text.RegularExpressions;
+using Ingame = Sandbox.ModAPI.Ingame;
+
+namespace Dondelium.crittanks{
+  [MySessionComponentDescriptor(MyUpdateOrder.AfterSimulation)]
+  public class PlanetStorage : MySessionComponentBase{
+    public static Dictionary<long, MyPlanet> planets = new Dictionary<long, MyPlanet>();
+    private int skip = 0;
+    private const int SKIP_TICKS = 180;
+    
+    private static HashSet<IMyEntity> ents = new HashSet<IMyEntity>();
+
+    //Admin values.
+    private bool init = false;
+    public static PlanetStorage instance;
+
+    public void Init(){}
+
+    public override void UpdateAfterSimulation(){
+      if(!init){
+        init = true;
+        instance = this;
+      }
+
+      if(++skip >= SKIP_TICKS){
+        skip = 0;
+        MyAPIGateway.Entities.GetEntities(ents, delegate(IMyEntity e){
+          if(e is MyPlanet){
+            if(!PlanetStorage.planets.ContainsKey(e.EntityId)){
+              PlanetStorage.planets.Add(e.EntityId, e as MyPlanet);
+            }
+          }
+          return false;
+        });
+      }
+    }
+
+    protected override void UnloadData(){
+      planets.Clear();
+      ents.Clear();
+    }
+  }
+}
